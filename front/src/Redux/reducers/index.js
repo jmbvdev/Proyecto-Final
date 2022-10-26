a65ce24be9c72bb9c02803172a772605a358af2e
Tomas Falchini <tomasfalchini@gmail.com>
2022-10-26 15:44:36 -0300

    redux for cart

--- front/src/Redux/reducers/index.js
+++ front/src/Redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import productsReducer from "./products/index.js";
 import usersReducer from "./users/index.js";
+import shopCartReducer from "./shopCart/index.js";
 
 const rootReducer = combineReducers({
   productsReducer,
   usersReducer,
+  shopCartReducer,
 });
 
 export default rootReducer;
