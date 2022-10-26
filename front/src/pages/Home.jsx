884257c1250d3f6b57dfc8ccd3861d53fbdc069f
jmbvdev <jmvb.dev@gmail.com>
2022-10-26 14:45:44 -0400

    styles

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import HomeList from '../components/HomeList';
 import PopularPlants from '../components/PopularPlants';
-import SearchBox from '../components/SearchBox';
+
 
 
 const Home = () => {
