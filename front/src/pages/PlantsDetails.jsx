6017cab48c9f5e5874e2dc4c67580e13caddda74
Tomas Falchini <tomasfalchini@gmail.com>
2022-10-27 19:56:08 -0300

    changes

--- front/src/pages/PlantsDetails.jsx
+++ front/src/pages/PlantsDetails.jsx
@@ -18,7 +18,6 @@ const PlantsDetails = () => {
     (state) => state.productsReducer.productDetails.data
   );
 
-
   const id = useParams().id;
 
   const [quantity, setQuantity] = useState(1);
@@ -41,68 +40,64 @@ const PlantsDetails = () => {
     );
   }
 
-  return (
-    
-        plant?.name ? <div className={s.container}>
-        <img src={plant?.image} alt="" />
-        <div className={s.details}>
-          <h1>{plant?.name} </h1>
+  return plant?.name ? (
+    <div className={s.container}>
+      <img src={plant?.image} alt="" />
+      <div className={s.details}>
+        <h1>{plant?.name} </h1>
+        <div>
+          <h4>Description</h4>
+          <p>{plant?.details}</p>
+        </div>
+        <div className={s.categories}>
+          {plant?.categories &&
+            plant.categories?.map((p, i) => {
+              return (
+                <div key={i} className={s.categories_container}>
+                  {p.includes("tabletop") ? (
+                    <GiTable className={s.table} />
+                  ) : p.includes("pet friendly") ? (
+                    <FaDog className={s.table} />
+                  ) : (
+                    p.includes("easy care") && <TbPlant2 className={s.table} />
+                  )}
+                  <span>
+                    {p.includes("tabletop")
+                      ? "TABLETOP"
+                      : p.includes("pet friendly")
+                      ? "PET FRIENDLY"
+                      : p.includes("easy care") && "EASY CARE"}
+                  </span>
+                </div>
+              );
+            })}
+        </div>
+        <div className={s.price}>
           <div>
-            <h4>Description</h4>
-            <p>{plant?.details}</p>
+            <h4>Price</h4>
+            <h3>$ {plant?.price}</h3>
           </div>
-          <div className={s.categories}>
-            {plant?.categories &&
-              plant.categories?.map((p, i) => {
-                return (
-                  <div key={i} className={s.categories_container}>
-                    {p.includes("tabletop") ? (
-                      <GiTable className={s.table} />
-                    ) : p.includes("pet friendly") ? (
-                      <FaDog className={s.table} />
-                    ) : (
-                      p.includes("easy care") && (
-                        <TbPlant2 className={s.table} />
-                      )
-                    )}
-                    <span>
-                      {p.includes("tabletop")
-                        ? "TABLETOP"
-                        : p.includes("pet friendly")
-                        ? "PET FRIENDLY"
-                        : p.includes("easy care") && "EASY CARE"}
-                    </span>
-                  </div>
-                );
-              })}
+          <div className={s.quantity}>
+            <button onClick={() => setQuantity(quantity - 1)}>-</button>
+            <p>{quantity < 1 ? 0 : quantity}</p>
+            <button onClick={() => setQuantity(quantity + 1)}>+</button>
           </div>
-          <div className={s.price}>
-            <div>
-              <h4>Price</h4>
-              <h3>$ {plant?.price}</h3>
-            </div>
-            <div className={s.quantity}>
-              <button onClick={() => setQuantity(quantity - 1)}>-</button>
-              <p>{quantity < 1 ? 0 : quantity}</p>
-              <button onClick={() => setQuantity(quantity + 1)}>+</button>
-            </div>
-          </div>
-          <div className={s.favorites}>
-            <h4>Add to favorites</h4>
-            <button>
-              <AiFillHeart className={s.hearth} />
-            </button>
-          </div>
-
-          <button onClick={handleCart} className={s.cart}>
-            Add to Cart
+        </div>
+        <div className={s.favorites}>
+          <h4>Add to favorites</h4>
+          <button>
+            <AiFillHeart className={s.hearth} />
           </button>
         </div>
-      </div>:
-      <Loading/>
-    
-    
-  )
+
+        <button onClick={handleCart} className={s.cart}>
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
 };
 
 export default PlantsDetails;
