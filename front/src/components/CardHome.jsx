8938146d524bf5a7c958e5b2f19751ea3893af15
jmbvdev <jmvb.dev@gmail.com>
2022-10-27 12:58:15 -0400

    conecting home

--- front/src/components/CardHome.jsx
+++ front/src/components/CardHome.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import s from "../styles/cardHome.module.css"
 
 const CardHome = ({plant}) => {
+    const navigate= useNavigate()
     return (
         <div className={s.container_card}>
                     <div className={s.card}>
                     <div className={s.card_overlay}>
-                            <button>more details</button>
+                            <button onClick={()=>navigate(`/plants/details/${plant.id}`)}>more details</button>
                         </div>
-                        <img src={plant.image} alt="" />
+                        <img src={plant.data.image} alt="" />
                         <div className={s.specs}>
-                            <p>{plant.name}</p>
-                            <b>$65</b>
-                      
-                    
+                            <p>{plant.data.name}</p>
+                            <b>$ {plant.data.price}</b>
                         </div>
                     </div>
                 </div>
