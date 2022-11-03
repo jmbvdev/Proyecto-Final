29a5557e7b84ce78c487e805bcaf28ceb3e8f8df
Cristian Murua <cristianmurua1995@gmail.com>
2022-11-03 04:15:13 -0300

    react table first commit

--- front/src/auth/Verification.jsx
+++ front/src/auth/Verification.jsx
@@ -4,31 +4,38 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { signOut, sendEmailVerification } from "firebase/auth";
 import { setCurrentUser } from "../Redux/actions/users";
+import Swal from "sweetalert2"
 
 function Verification() {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleVerify = (e) => {
-        e.preventDefault();
-        
-            sendEmailVerification(auth.currentUser).then( () => {
-               signOut(auth).then(() => {
-                dispatch(setCurrentUser(null));
-                navigate("/login");
-              }
-              )
-            });
-    
-}
+    React.useEffect(() => {
+        sendEmailVerification(auth.currentUser).then( () => {
+                Swal.fire({
+                  title:"Account verification required!",
+                  text:"Verification sended! Check your email box",
+                  icon:"warning",
+                  showDenyButton:false,
+                  denyButtonText:"No",
+                  denyButtonColor:"#FF5733",
+                  confirmButtonText:"OK",
+                  confirmButtonColor:"#72CE65"
+                }).then(res=>{
+                  if (res.isConfirmed) {
+                      signOut(auth).then(() => {
+                      dispatch(setCurrentUser(null))
+                      navigate('/sign-in');
+                  });
+                  }
+                })
+              });
+    }, [])
 
 
     return (
         <div>
-            <h3>Your account is not verify!</h3>
-            <p>To proceed click the following button!</p>
-            <button onClick={handleVerify}>send verification link</button>
         </div>
     )
 }
