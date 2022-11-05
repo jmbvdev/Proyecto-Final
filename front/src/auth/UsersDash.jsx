9443a7da879c863267cfc4da4784a4004769cae0
Tomas Falchini <tomasfalchini@gmail.com>
2022-11-05 14:29:35 -0300

    users dash 4

--- front/src/auth/UsersDash.jsx
+++ front/src/auth/UsersDash.jsx
@@ -1,8 +1,94 @@
 import React from "react";
-import { setCurrentUser, getUsers, getUserDetail, deleteUser } from "../Redux/actions/users";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setCurrentUser,
+  getUsers,
+  getUserDetail,
+  deleteUser,
+} from "../Redux/actions/users";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase/firebase";
+import { useTable } from "react-table";
 
-const UserDetail = () => {
+const AdminNav = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const allUsers = useSelector((state) => state.usersReducer.users);
+  const admin = auth.currentUser;
 
-}
+  React.useEffect(() => {
+    dispatch(getUsers());
+  }, []);
 
-export default UserDetail
+  const Table = ({ columns, data }) => {
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+      useTable({ columns, data });
+
+    return (
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  };
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "User",
+        columns: [
+          {
+            Header: "Display Name",
+            accessor: "displayName",
+          },
+          {
+            Header: "Email",
+            accessor: "email",
+          },
+        ],
+      },
+      {
+        Header: "Info",
+        columns: [
+          {
+            Header: "Phone Number",
+            accessor: "phoneNumber",
+          },
+        ],
+      },
+    ],
+    []
+  );
+
+  const data = allUsers;
+
+  return (
+    <div>
+      <h2>Welcome {admin.displayName}!</h2>
+      <Table columns={columns} data={data} />
+    </div>
+  );
+};
+
+export default AdminNav;
