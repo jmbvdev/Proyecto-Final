ebc59ada2399038ab82cb736440ccd0eec8e91d3
jmbvdev <jmvb.dev@gmail.com>
2022-11-05 19:38:03 -0400

    fix Reviews

--- front/src/components/OrdersCard.jsx
+++ front/src/components/OrdersCard.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const OrdersCard = (props) => {
-    console.log(props.date);
+ 
     const navigate = useNavigate()
 
     return (
