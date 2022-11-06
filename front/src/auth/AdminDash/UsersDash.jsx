413656febf999b655be5f530ed00d81418ba19b3
jmbvdev <jmvb.dev@gmail.com>
2022-11-05 20:52:34 -0400

    fix usersDash

--- front/src/auth/AdminDash/UsersDash.jsx
+++ front/src/auth/AdminDash/UsersDash.jsx
@@ -51,21 +51,17 @@ const UsersDash = () => {
             </tr>
           ))}
         </thead>
-        <tbody {...getTableBodyProps()}>
+        <tbody {...getTableBodyProps()} >
           {rows.map((row, i) => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
                 {row.cells.map((cell) => {
-                  return (
-                    <tr {...row.getRowProps()} >
-                      {row.cells.map(cell => {
                         return <td  {...cell.getCellProps()}>{cell.render('Cell')}</td>
                       })}
                     </tr>
                   )
                 })}
-                </tr>)})}
               </tbody>
             </table>
           )
