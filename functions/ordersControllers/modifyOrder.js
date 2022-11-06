2aa75ab533d5c8fa0768ab4ba81ac1fbf412a0f1
Tomas Falchini <tomasfalchini@gmail.com>
2022-11-06 01:31:23 -0300

    email post purchase

--- functions/ordersControllers/modifyOrder.js
+++ functions/ordersControllers/modifyOrder.js
@@ -1,5 +1,6 @@
 const { db, admin } = require("../config/firebase.js");
 const nodemailer = require("nodemailer");
+import logo from "../../front/src/images/logo-sinfondo.png";
 
 let transporter = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
@@ -38,9 +39,18 @@ module.exports = async function postOrder(orderid, cart, state, extras, email) {
       from: "Calathea Markets <tom_cremoso@hotmail.com>",
       to: email,
       subject: "Thank you so much",
-      html: `<p style="font-size: 16px;">Your order at Calathea Market was aprobed</p>
+      html: `<p style="font-size: 25px;">Your order at Calathea Market was ${state}</p>
                 <br />
-                <img src=${cart[0].image} />
+                <img style="weight: 400px; height: 300px;" src=${logo} alt="" />
+                <p style="font-size: 20px;">Your order id is ${orderid}.</p>
+                <p style="font-size: 16px;">We are preparing your package, in the next 24 hours it would be ok to send or retire.</p>
+                <br />
+                <br />
+                <p style="font-size: 16px;">Please, feel free to let a feedback about the products you get, so other people can know more about us.</p>
+                <br />
+                <br />
+                <p style="font-size: 16px;">Lots of love, Calathea Team.</p>
+                
             `,
     };
 
