4e2d760941f36e6a67d49060f19744da57e7cad7
Tomas Falchini <tomasfalchini@gmail.com>
2022-11-13 14:25:13 -0300

    corrections to stadistics

--- front/src/components/SalesCount.jsx
+++ front/src/components/SalesCount.jsx
@@ -46,6 +46,7 @@ function SalesCount() {
         sales[i].date <= e.target.value.split(",")[1]
       ) {
         let date = new Date(sales[i].date).toDateString();
+
         if (data.hasOwnProperty(date)) {
           data[date] = data[date] + 1;
         } else data[date] = 1;
@@ -63,11 +64,11 @@ function SalesCount() {
       </button>
       <h3>SALES COUNT</h3>
       <select onChange={handleOnPeriod}>
-        <option value="2022-09-10,2022-09-15">09/10 - 15/10</option>
-        <option value="2022-10-16,2022-10-22">16/10 - 22/10</option>
-        <option value="2022-10-23,2022-10-29">23/10 - 29/10</option>
-        <option value="2022-10-30,2022-11-06">30/10 - 06/11</option>
-        <option value="2022-11-07,2022-11-13">07/11 - 13/11</option>
+        <option value="2022-09-10,2022-09-16">09/10 - 15/10</option>
+        <option value="2022-10-16,2022-10-23">16/10 - 22/10</option>
+        <option value="2022-10-23,2022-10-30">23/10 - 29/10</option>
+        <option value="2022-10-30,2022-11-07">30/10 - 06/11</option>
+        <option value="2022-11-07,2022-11-14">07/11 - 13/11</option>
         <option value="2022-11-14,2022-11-20">14/11 - 20/11</option>
       </select>
       <PieChart data={period} empty="No sales for the stipulated period" />
